fc9aaf14a4639e24dac043331d551f178d442dca
philipe <philipedarosa10@gmail.com>
2025-09-23 13:34:38 -0300

    ract

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name ='philipe'
+
+  const newName = name.toLocaleUpperCase()
+
+  function soma (a, b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
     <h1>Ola react</h1>
+    <h2>alterando jsx</h2>
     <p>app</p>
+    <p>ola {newName}</p>
+    <p>soma {soma(1,7)} </p>
+    <img src={url} alt="minha imagem" />
     </div>
   );
 }
