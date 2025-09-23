beaf1c62d793e2e5ed9065347be0dff2c9d821df
philipe <philipedarosa10@gmail.com>
2025-09-22 22:25:32 -0300

    react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Ola react</h1>
+    <p>app</p>
+    </div>
+  );
+}
+
+export default App;
