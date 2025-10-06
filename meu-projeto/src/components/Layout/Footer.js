64c6c1ddfc329afda7eca7dde38225985b967e6f
philipe <philipedarosa10@gmail.com>
2025-10-06 13:21:13 -0300

    react

--- meu-projeto/src/components/Layout/Footer.js
+++ meu-projeto/src/components/Layout/Footer.js
@@ -1,7 +1,25 @@
-function Footer(){
-     
-    return <footer> Rodapé </footer>
-    
+import { FaFacebook, FaInstagram, FaLink, FaLinkedin } from 'react-icons/fa';
+import styles from './Footer.module.css'
+
+
+function Footer() {
+
+    return (
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+
+            <p>rodape</p>
+
+        </footer>
+
+
+    )
+
+
 }
 
 export default Footer;
