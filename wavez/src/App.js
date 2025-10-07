5fb91ccd35f1adfdc2b49172bb532f9f525ea5ed
philipe <philipedarosa10@gmail.com>
2025-10-07 13:30:49 -0300

    react

--- wavez/src/App.js
+++ wavez/src/App.js
@@ -1,11 +1,32 @@
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
-    <div className="App">
-   <p>  Wavez</p>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/company'>Company</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+          <li><Link to='/newproject'>New Project</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/newproject' element={<NewProject />} />
+      </Routes>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </Router>
   );
 }
 
---
+++ wavez/src/components/pages/NewProject.js
@@ -0,0 +1,6 @@
+function NewProject(){
+return <h1>New Project</h1>
+
+}
+
+export default NewProject;
