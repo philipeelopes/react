e019f5e4f1415c5f7446db8c66a9ab42cd80266a
philipe <philipedarosa10@gmail.com>
2025-10-10 13:26:32 -0300

    react

---
+++ wavez/src/components/form/Input.js
@@ -0,0 +1,13 @@
+import styles from './Input.module.css';
+
+function Input({type, text, name, placeholder, handleOnChange, value}) {
+return(
+    <div className={styles.form_control}>
+        <label htmlFor=""></label>
+        <input type="text"></input>
+    </div>
+)
+
+}
+
+export default Input;
